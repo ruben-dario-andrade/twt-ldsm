84beb173c21fe622fbd8f1225aff0b49941e58e3
ruben-dario-andrade <71405439+ruben-dario-andrade@users.noreply.github.com>
2022-04-14 19:38:11 -0500

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -76,7 +76,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
