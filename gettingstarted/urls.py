7c89bcb0e95f5b2763bc7a588dba243416cd3b41
ruben-dario-andrade <71405439+ruben-dario-andrade@users.noreply.github.com>
2022-04-14 19:07:14 -0500

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,16 +6,8 @@ admin.autodiscover()
 
 import ldsmtweet.views
 
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
     path("", ldsmtweet.views.index, name="index"),
-    path("db/", ldsmtweet.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
